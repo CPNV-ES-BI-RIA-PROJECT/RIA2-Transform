1a320040ad0c0a487cfade43b9fdb36716d81a1b
Nicolas Glassey <nicolas.glassey@eduvaud.ch>
2026-04-01 21:48:18 +0200

    ci(workflow): add helper for image deployment

---
+++ ms-bucket-adapter/deploy/prod-promotion-workflow.sh
@@ -0,0 +1,3 @@
+docker pull ghcr.io/cpnv-es-bi-ria-project/ria2-transform-bucket-adapter:stage
+docker tag ghcr.io/cpnv-es-bi-ria-project/ria2-transform-bucket-adapter:prod
+docker push ghcr.io/cpnv-es-bi-ria-project/ria2-transform-bucket-adapter:prod
--- ms-bucket-adapter/deploy/stage-publish-image.sh
+++ ms-bucket-adapter/deploy/stage-publish-image.sh
@@ -1,4 +1,4 @@
 docker buildx create --use
 docker buildx build --platform linux/amd64,linux/arm64 \
-  -t ghcr.io/cpnv-es-bi-ria-project/ria2-transform:latest \
+  -t ghcr.io/cpnv-es-bi-ria-project/ria2-transform-bucket-adapter:stage \
   --push .
